07bb37b87c36926e994c11555a43b250a0302e0c
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-10 16:43:08 -0300

    implements tuple addition

--- sources/tuples/tuple.c
+++ sources/tuples/tuple.c
@@ -30,3 +30,16 @@ char tuplecmp(t_tuple *t1, t_tuple *t2)
     }
     return (result);
 }
+
+t_tuple *tupleadd(t_tuple *t1, t_tuple *t2)
+{
+    t_tuple *result;
+
+    result = new_tuple(
+        t1[0] + t2[0],
+        t1[1] + t2[1],
+        t1[2] + t2[2],
+        t1[3] + t2[3]
+    );
+    return result;
+}
