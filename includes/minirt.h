277394343410987db27dbe6cf673878e1c5f4176
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-10 17:08:32 -0300

    implements tuple subtraction

--- includes/minirt.h
+++ includes/minirt.h
@@ -9,6 +9,7 @@ typedef t_tuple t_vector;
 
 t_tuple *new_tuple(float x, float y, float z, float type);
 t_tuple *tupleadd(t_tuple *t1, t_tuple *t2);
+t_tuple *tuplesub(t_tuple *t1, t_tuple *t2);
 t_tuple *new_point(float x, float y, float z);
 t_vector *new_vector(float x, float y, float z);
 char    tuplecmp(t_tuple *t1, t_tuple *t2);
