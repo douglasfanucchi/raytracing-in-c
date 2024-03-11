12877a88252867588012f256d0e7f6e399406051
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-10 23:19:35 -0300

    implements tupledivide

--- includes/minirt.h
+++ includes/minirt.h
@@ -11,6 +11,7 @@ t_tuple *new_tuple(float x, float y, float z, float type);
 t_tuple *tupleadd(t_tuple *t1, t_tuple *t2);
 t_tuple *tuplesub(t_tuple *t1, t_tuple *t2);
 t_tuple *tuplemultiply(t_tuple *tuple, float scalar);
+t_tuple *tupledivide(t_tuple *tuple, float scalar);
 t_tuple *tuple_opposite(t_tuple *tuple);
 t_tuple *new_point(float x, float y, float z);
 t_vector *new_vector(float x, float y, float z);
--- sources/tuples/scalar.c
+++ sources/tuples/scalar.c
@@ -9,3 +9,8 @@ t_tuple *tuplemultiply(t_tuple *tuple, float scalar)
         tuple[3] * scalar
     );
 }
+
+t_tuple *tupledivide(t_tuple *tuple, float scalar)
+{
+    return tuplemultiply(tuple, 1.f/scalar);
+}
