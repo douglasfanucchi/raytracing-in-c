30b19db190b79a0271e58714f19385ff84cac99c
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-10 23:19:22 -0300

    implements test to divide vector by scalar

--- tests/tuples/scalar.c
+++ tests/tuples/scalar.c
@@ -11,6 +11,18 @@ MU_TEST(test_multiply) {
     );
 }
 
+MU_TEST(test_divide) {
+    t_tuple *tuple = new_tuple(10, 5, 3, 1);
+    t_tuple *expected = new_tuple(5, 2.5, 1.5, 0.5);
+    t_tuple *result = tupledivide(tuple, 2);
+
+    mu_assert(
+        tuplecmp(expected, result) == 0,
+        "result should be (5, 2.5, 1.5, 0.5)"
+    );
+}
+
 MU_TEST_SUITE(test_scalar) {
     MU_RUN_TEST(test_multiply);
+    MU_RUN_TEST(test_divide);
 }
