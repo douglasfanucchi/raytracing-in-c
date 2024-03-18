3c459327fbcf81d25e9fb5d1db9e40b9404dd672
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-18 02:41:50 -0300

    add test case to dot product

--- tests/tuples/dot.c
+++ tests/tuples/dot.c
@@ -19,7 +19,16 @@ MU_TEST(test_dot_product_should_return_the_angles_cosine_when_unit_vectors) {
     mu_assert_double_eq(expected, result);
 }
 
+MU_TEST(test_dot_product_should_return_1_when_same_vectors_when_unit_vectors) {
+    t_vector *v1 = new_vector(1, 0, 0);
+    float result = dot(v1, v1);
+    float expected = 1;
+
+    mu_assert_double_eq(expected, result);
+}
+
 MU_TEST_SUITE(test_dot_product) {
     MU_RUN_TEST(test_dot_product_should_return_0_when_angle_between_vectors_is_90);
     MU_RUN_TEST(test_dot_product_should_return_the_angles_cosine_when_unit_vectors);
+    MU_RUN_TEST(test_dot_product_should_return_1_when_same_vectors_when_unit_vectors);
 }
