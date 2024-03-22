2ed369a899a8b332e1d654c9e13f29a03179daef
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-22 00:12:58 -0300

    adds test to multiply colors

--- tests/tuples/color.c
+++ tests/tuples/color.c
@@ -43,9 +43,22 @@ MU_TEST(test_multiply_color_by_scalar) {
     );
 }
 
+MU_TEST(test_mutiply_colors) {
+    t_color *c1 = new_color(0.5, 0.1, 1);
+    t_color *c2 = new_color(0.3, 0.75, 0.9);
+    t_color *result = colorsmultiply(c1, c2);
+    t_color *expected = new_color(0.15, 0.075, 0.9);
+
+    mu_assert(
+        tuplecmp(expected, result) == 0,
+        "Result color should be (0.15, 0.075, 0.9)"
+    );
+}
+
 MU_TEST_SUITE(test_colors) {
     MU_RUN_TEST(test_should_create_color_successfully);
     MU_RUN_TEST(test_add_colors);
     MU_RUN_TEST(test_subtract_colors);
     MU_RUN_TEST(test_multiply_color_by_scalar);
+    MU_RUN_TEST(test_mutiply_colors);
 }
