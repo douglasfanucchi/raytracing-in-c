b7cb4532620a4910d66dc5f7679fd78c7efc6350
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-24 04:56:38 -0300

    implements test to matricesmultiply

--- tests/matrix/matrix.c
+++ tests/matrix/matrix.c
@@ -138,6 +138,70 @@ MU_TEST(test_matrixcmp_to_different_matrices) {
     );
 }
 
+MU_TEST(test_multiply_matrices) {
+    t_matrix *A = new_matrix(4);
+    t_matrix *B = new_matrix(4);
+
+    A->values[0][0] = 1;
+    A->values[0][1] = 2;
+    A->values[0][2] = 3;
+    A->values[0][3] = 4;
+    A->values[1][0] = 5;
+    A->values[1][1] = 6;
+    A->values[1][2] = 7;
+    A->values[1][3] = 8;
+    A->values[2][0] = 9;
+    A->values[2][1] = 8;
+    A->values[2][2] = 7;
+    A->values[2][3] = 6;
+    A->values[3][0] = 5;
+    A->values[3][1] = 4;
+    A->values[3][2] = 3;
+    A->values[3][3] = 2;
+
+    B->values[0][0] = -2;
+    B->values[0][1] = 1;
+    B->values[0][2] = 2;
+    B->values[0][3] = 3;
+    B->values[1][0] = 3;
+    B->values[1][1] = 2;
+    B->values[1][2] = 1;
+    B->values[1][3] = -1;
+    B->values[2][0] = 4;
+    B->values[2][1] = 3;
+    B->values[2][2] = 6;
+    B->values[2][3] = 5;
+    B->values[3][0] = 1;
+    B->values[3][1] = 2;
+    B->values[3][2] = 7;
+    B->values[3][3] = 8;
+
+    t_matrix *result = matricesmultiply(A, B);
+    t_matrix *expected = new_matrix(4);
+
+    expected->values[0][0] = 20;
+    expected->values[0][1] = 22;
+    expected->values[0][2] = 50;
+    expected->values[0][3] = 48;
+    expected->values[1][0] = 44;
+    expected->values[1][1] = 54;
+    expected->values[1][2] = 114;
+    expected->values[1][3] = 108;
+    expected->values[2][0] = 40;
+    expected->values[2][1] = 58;
+    expected->values[2][2] = 110;
+    expected->values[2][3] = 102;
+    expected->values[3][0] = 16;
+    expected->values[3][1] = 26;
+    expected->values[3][2] = 46;
+    expected->values[3][3] = 42;
+
+    mu_assert(
+        matrixcmp(expected, result) == 0,
+        "Result matrix should be equal the expected matrix"
+    );
+}
+
 MU_TEST_SUITE(test_matrix) {
     MU_RUN_TEST(test_2x2_matrix_creation);
     MU_RUN_TEST(test_3x3_matrix_creation);
@@ -145,4 +209,5 @@ MU_TEST_SUITE(test_matrix) {
     MU_RUN_TEST(test_matrixcmp_to_different_dimensions_matrices);
     MU_RUN_TEST(test_matrixcmp_to_equal_matrices);
     MU_RUN_TEST(test_matrixcmp_to_different_matrices);
+    MU_RUN_TEST(test_multiply_matrices);
 }
