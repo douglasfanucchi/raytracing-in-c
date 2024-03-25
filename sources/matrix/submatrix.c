ad5995dab46391e9a6c5717f43e4128af6cb8a43
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-25 12:33:12 -0300

    fixes side effect on calculating submatrix

--- sources/matrix/submatrix.c
+++ sources/matrix/submatrix.c
@@ -5,21 +5,23 @@ t_matrix *submatrix(t_matrix *A, int row, int column)
     t_matrix    *result;
     int         i;
     int         j;
-    float       **values;
+    float       **rows;
+    float       *columns;
 
     result = new_matrix(A->dimension - 1);
-    values = A->values;
+    rows = A->values;
     i = 0;
     while (i < result->dimension)
     {
         if (i == row)
-            values++;
+            rows++;
+        columns = rows[i];
         j = 0;
         while (j < result->dimension)
         {
             if (j == column)
-                values[i]++;
-            result->values[i][j] = values[i][j];
+                columns++;
+            result->values[i][j] = columns[j];
             j++;
         }
         i++;
