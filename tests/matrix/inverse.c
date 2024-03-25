484fdda4d375207a54c0e20ab5dc8a1a9c3e94f3
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-25 15:11:45 -0300

    adds test case to is_invertible

--- tests/matrix/inverse.c
+++ tests/matrix/inverse.c
@@ -11,6 +11,17 @@ MU_TEST(test_is_invertible) {
         is_invertible(A) == 0,
         "Matrix A should not be considered invertible"
     );
+
+    A = new_matrix(2);
+    A->values[0][0] = 1;
+    A->values[0][1] = 2;
+    A->values[1][0] = 3;
+    A->values[1][1] = 4;
+
+    mu_assert(
+        is_invertible(A) == 1,
+        "Matrix A should be considered invertible"
+    );
 }
 
 MU_TEST_SUITE(test_inverse_matrix) {
