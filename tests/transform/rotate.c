b7882e886e92961946a6dd628d6ac0ad6c0fa29f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-27 10:53:20 -0300

    adds test to rotate around y axis

--- tests/transform/rotate.c
+++ tests/transform/rotate.c
@@ -12,6 +12,19 @@ MU_TEST(test_rotate_around_x_axis) {
     );
 }
 
+MU_TEST(test_rotate_around_y_axis) {
+    t_point *p = new_point(0, 0, 1);
+    t_matrix *transformation = rotatey(M_PI_2);
+    t_point *result = matrixtuplemultiply(transformation, p);
+    t_point *expected = new_point(1, 0, 0);
+
+    mu_assert(
+        tuplecmp(expected, result) == 0,
+        "Result should be the point at (1, 0, 0)"
+    );
+}
+
 MU_TEST_SUITE(test_rotate_matrices) {
     MU_RUN_TEST(test_rotate_around_x_axis);
+    MU_RUN_TEST(test_rotate_around_y_axis);
 }
