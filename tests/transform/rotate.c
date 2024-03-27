c64c7c0d119c7bbdfa7bc225833b26906782b9c9
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-03-27 11:04:16 -0300

    Implements test to rotate around z axis

--- tests/transform/rotate.c
+++ tests/transform/rotate.c
@@ -24,7 +24,20 @@ MU_TEST(test_rotate_around_y_axis) {
     );
 }
 
+MU_TEST(test_rotate_around_z_axis) {
+    t_point *p = new_point(0, 1, 0);
+    t_matrix *transformation = rotatez(M_PI_2);
+    t_point *result = matrixtuplemultiply(transformation, p);
+    t_point *expected = new_point(-1, 0, 0);
+
+    mu_assert(
+        tuplecmp(expected, result) == 0,
+        "Result should be the point at (-1, 0, 0)"
+    );
+}
+
 MU_TEST_SUITE(test_rotate_matrices) {
     MU_RUN_TEST(test_rotate_around_x_axis);
     MU_RUN_TEST(test_rotate_around_y_axis);
+    MU_RUN_TEST(test_rotate_around_z_axis);
 }
