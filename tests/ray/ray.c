b63c85d012269e0e60a103a1e7a6a0b35d27ffb5
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-04-20 15:43:50 -0300

    implements test to computate ray position

--- tests/ray/ray.c
+++ tests/ray/ray.c
@@ -16,6 +16,18 @@ MU_TEST(test_ray_creation) {
     );
 }
 
+MU_TEST(test_ray_position_given_a_certain_scalar) {
+    t_ray *ray = new_ray(new_point(2, 3, 4), new_vector(1, 0, 0));
+
+    mu_check(
+        tuplecmp(position(ray, 0), ray->origin) == 0 &&
+        tuplecmp(position(ray, 1), new_point(3, 3, 4)) == 0 &&
+        tuplecmp(position(ray, -1), new_point(1, 3, 4)) == 0 &&
+        tuplecmp(position(ray, 2.5), new_point(4.5, 3, 4)) == 0
+    );
+}
+
 MU_TEST_SUITE(test_ray) {
     MU_RUN_TEST(test_ray_creation);
+    MU_RUN_TEST(test_ray_position_given_a_certain_scalar);
 }
