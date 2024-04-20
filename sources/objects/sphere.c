30adac7b855f3ed5fa49cfcf97b4141b6440e58d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-04-20 16:13:58 -0300

    implements sphere creation

---
+++ sources/objects/sphere.c
@@ -0,0 +1,18 @@
+#include <minirt.h>
+
+t_sphere *new_sphere(t_point *origin, float radius)
+{
+    t_sphere    *sphere;
+
+    sphere = malloc(sizeof(t_sphere));
+    sphere->origin = origin;
+    sphere->radius = radius;
+
+    return (sphere);
+}
+
+void free_sphere(t_sphere *sphere)
+{
+    free(sphere->origin);
+    free(sphere);
+}
