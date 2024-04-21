c5e1d26a8c6eba336b14c11c0bb4a7f2b03c9681
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-04-21 01:19:03 -0300

    implements ray intersection on a sphere

--- sources/ray/intersections.c
+++ sources/ray/intersections.c
@@ -10,6 +10,35 @@ t_intersection *new_intersection(float t, void *object)
     return (intersection);
 }
 
+t_list  **intersect(void *object, t_ray *ray)
+{
+    float           discriminant;
+    t_vector        *sphere_to_ray;
+    t_list          **list;
+    float           a;
+    float           b;
+    float           c;
+
+    list = ft_newlist();
+    sphere_to_ray = tuplesub(ray->origin, ((t_sphere *)object)->origin);
+    a = dot(ray->direction, ray->direction);
+    b = dot(sphere_to_ray, ray->direction);
+    c = dot(sphere_to_ray, sphere_to_ray) - pow(((t_sphere *)object)->radius, 2);
+    discriminant = pow(b, 2) - a*c;
+    if (!(discriminant < 0)) {
+        ft_lstadd_back(
+            list,
+            ft_lstnew(new_intersection((-b - sqrt(discriminant)) / a, object))
+        );
+        ft_lstadd_back(
+            list,
+            ft_lstnew(new_intersection((-b + sqrt(discriminant)) / a, object))
+        );
+    }
+    free(sphere_to_ray);
+    return (list);
+}
+
 void free_intersection(t_intersection *intersection)
 {
     free(intersection);
