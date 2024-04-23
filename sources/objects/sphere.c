6996d8f351eb1d30a750289949b24ff45844aeb6
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-04-23 06:40:51 -0300

    implements sphere material property

--- sources/objects/sphere.c
+++ sources/objects/sphere.c
@@ -6,11 +6,15 @@ t_sphere *new_sphere()
 
     sphere = malloc(sizeof(t_sphere));
     sphere->transform = new_identity();
+    sphere->material = new_material(
+        new_color(1, 1, 1), 0.1, 0.9, 0.9, 200
+    );
     return (sphere);
 }
 
 void free_sphere(t_sphere *sphere)
 {
+    free_material(sphere->material);
     free_matrix(sphere->transform);
     free(sphere);
 }
